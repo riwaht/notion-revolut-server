eb70dafae6e55e72b97f2076ad2154ca093e58a0
Riwa Hoteit <riwa.hoteit@gmail.com>
2025-08-17 14:09:05 +0200

    Remove authentication for Make

--- app.py
+++ app.py
@@ -3,23 +3,15 @@ from fastapi.responses import JSONResponse
 from notion_revolut_connector import main as revolut_sync
 import uvicorn
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 app = FastAPI()
-API_KEY = os.getenv("API_KEY", "default-if-not-set")
 
 @app.get("/")
 def root():
     return {"status": "Revolut–Notion Sync Server is live."}
 
 @app.post("/sync")
-async def sync(request: Request):
-    # Optional: simple auth
-    auth = request.headers.get("authorization", "")
-    if auth != f"Bearer {API_KEY}":
-        raise HTTPException(status_code=403, detail="Unauthorized")
-
+async def sync():
     try:
         result = revolut_sync()
         return {"status": "success", "result": result}
